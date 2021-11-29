90a49f361598b4a70cf3d147ff8b1d18d0064c5e
Emalongi <91636607+Emalongi@users.noreply.github.com>
2021-11-29 14:08:43 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,15 +1,15 @@
-include "lib.h"
+#include "lib.h"
 
 bool conversione (char &a) {
     if ((a >= 65 and a <= 90) or (a >= 97 and a <= 122)) {
         if(a>=65 and a<=90){
             a=a+32;
-            return true;
+            return a;
         }else if(a>=97 and a<= 122){
             a=a-32;
-            return true;
+            return a;
         }return true;
     } else {
-        return false
+        return false;
     }
 }
