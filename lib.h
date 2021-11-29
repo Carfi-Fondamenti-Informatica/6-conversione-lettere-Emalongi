5ca890ab8876abfe0f305c471fb511d730e0ffb3
Emalongi <91636607+Emalongi@users.noreply.github.com>
2021-11-29 13:51:27 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -2,7 +2,7 @@
 // Created by Utente on 29/11/2021.
 //
 bool conversione (char &a);
-#ifndef ESERCITAZIONE6_LIB_H
-#define ESERCITAZIONE6_LIB_H
+#ifndef 6-conversione-lettere-Emalongi_LIB_H
+#define 6-conversione-lettere-Emalongi_LIB_H
 
-#endif //ESERCITAZIONE6_LIB_H
+#endif //6-conversione-lettere-Emalongi_LIB_H
